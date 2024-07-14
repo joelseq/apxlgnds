b8d76e2966b16596b4283f18df6937e3d336df85
Joel Sequeira <joelseq96@gmail.com>
2024-07-14 02:44:55 -0400

    Add footer

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -10,3 +10,21 @@
 <PlausibleAnalytics domain="apxlgnds.com" apiHost="https://plausible.joelseq.app" />
 
 <slot></slot>
+
+<footer class="w-full text-center py-5 bg-slate-800">
+  <p class="text-gray-400">
+    Created by <a class="text-blue-500" href="https://x.com/@_joelseq" target="_blank"
+      >Joel Sequeira</a
+    >
+    | <a href="https://github.com/joelseq/apxlgnds">GitHub</a>
+  </p>
+</footer>
+
+<style>
+  footer a {
+    text-decoration: none;
+  }
+  footer a:hover {
+    text-decoration: underline;
+  }
+</style>
