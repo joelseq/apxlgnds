8a97937ed7a0de081b0ffa3fee16b146bf8a9650
Joel Sequeira <joelseq96@gmail.com>
2024-07-13 23:46:26 -0400

    Add plausible

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -2,8 +2,11 @@
   import '../app.css'
   import { dev } from '$app/environment'
   import { inject } from '@vercel/analytics'
+  import { PlausibleAnalytics } from '@accuser/svelte-plausible-analytics'
 
   inject({ mode: dev ? 'development' : 'production' })
 </script>
+
+<PlausibleAnalytics domain="apxlgnds.com" apiHost="https://plausible.joelseq.app" />
 
 <slot></slot>
